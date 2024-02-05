d336a027d0367cc75a4b69a7c15c31b2c78e5d28
maeldcls <maelduclos03@gmail.com>
2024-02-05 08:04:50 +0100

    dunno

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,20 +2,23 @@
 
 namespace App\Controller;
 
-
+use App\Entity\Game;
+use App\Form\GameType;
+use App\Form\SearchType;
+use App\Repository\GameRepository;
 use App\Service\ApiDataService;
-use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use App\Form\SearchType;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class GameController extends AbstractController
 {
-    #[Route('/', name: 'app_game')]
+    #[Route('/', name: 'app_game', methods: ['GET'])]
     public function index(Request $request, ApiDataService $apiDataService): Response
     {
-       
         $form = $this->createForm(SearchType::class);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
@@ -40,5 +43,62 @@ class GameController extends AbstractController
             'formSearch'=>$form->createView(),
    
         ]);
+    }
+
+    #[Route('/new', name: 'app_game_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $game = new Game();
+        $form = $this->createForm(GameType::class, $game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($game);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_game_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('game/new.html.twig', [
+            'game' => $game,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_game_show', methods: ['GET'])]
+    public function show(Game $game): Response
+    {
+        return $this->render('game/show.html.twig', [
+            'game' => $game,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_game_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Game $game, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(GameType::class, $game);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_game_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('game/edit.html.twig', [
+            'game' => $game,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_game_delete', methods: ['POST'])]
+    public function delete(Request $request, Game $game, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$game->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($game);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_game_index', [], Response::HTTP_SEE_OTHER);
     }
 }
